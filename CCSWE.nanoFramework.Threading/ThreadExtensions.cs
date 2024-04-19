22f04f89906a7814a0d53c30e46f50d3cf44edc8
Cory Charlton <ccharlton@pobox.com>
2024-04-18 21:56:47 -0700

    Updating XML doc

--- CCSWE.nanoFramework.Threading/ThreadExtensions.cs
+++ CCSWE.nanoFramework.Threading/ThreadExtensions.cs
@@ -11,6 +11,8 @@ namespace CCSWE.nanoFramework.Threading
         /// <summary>
         /// Calls <see cref="Thread.Join(int)"/> if not being called from the same thread.
         /// </summary>
+        /// <param name="thread">The <see cref="Thread"/> to join.</param>
+        /// <param name="millisecondsTimeout">The number of milliseconds to wait for the thread to terminate.</param>
         /// <returns>true if the thread has terminated; false if the thread has not terminated after the amount of time specified by the millisecondsTimeout parameter has elapsed or called from the same thread.</returns>
         public static bool TryJoin(this Thread thread, int millisecondsTimeout)
         {
@@ -32,6 +34,8 @@ namespace CCSWE.nanoFramework.Threading
         /// <summary>
         /// Calls <see cref="Thread.Join(TimeSpan)"/> if not being called from the same thread.
         /// </summary>
+        /// <param name="thread">The <see cref="Thread"/> to join.</param>
+        /// <param name="timeout">The amount of time to wait for the thread to terminate.</param>
         /// <returns>true if the thread has terminated; false if the thread has not terminated after the amount of time specified by the timeout parameter has elapsed or called from the same thread.</returns>
         public static bool TryJoin(this Thread thread, TimeSpan timeout)
         {
