9c6d2b0e4bdeaff55571952039c2443beee1cb37
Cory Charlton <CoryCharlton@users.noreply.github.com>
2024-04-18 22:08:48 -0700

    Added TimeSpan overloads to WaitHandles extensions (#43)
    * Updating XML doc
    
    * Added TimeSpan overloads

--- CCSWE.nanoFramework.Threading/WaitHandles.cs
+++ CCSWE.nanoFramework.Threading/WaitHandles.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 
 namespace CCSWE.nanoFramework.Threading
 {
@@ -7,6 +8,18 @@ namespace CCSWE.nanoFramework.Threading
     /// </summary>
     public static class WaitHandles
     {
+        /// <summary>
+        /// Waits for all the elements in the specified array to receive a signal, using an Int32 value to specify the time interval and specifying whether to exit the synchronization domain before the wait.
+        /// </summary>
+        /// <param name="millisecondsTimeout">The number of milliseconds to wait, or <see cref="Timeout.Infinite"/> (-1) to wait indefinitely.</param>
+        /// <param name="exitContext">true to exit the synchronization domain for the context before the wait (if in a synchronized context), and reacquire it afterward; otherwise, false.</param>
+        /// <param name="waitHandles">A WaitHandle array containing the objects for which the current instance will wait. This array cannot contain multiple references to the same object (duplicates).</param>
+        /// <returns>true when every element in waitHandles has received a signal; otherwise, false.</returns>
+        public static bool WaitAll(int millisecondsTimeout, bool exitContext, params WaitHandle[] waitHandles)
+        {
+            return WaitHandle.WaitAll(waitHandles, millisecondsTimeout, exitContext);
+        }
+
         /// <summary>
         /// Waits for all the elements in the specified array to receive a signal.
         /// </summary>
@@ -20,13 +33,25 @@ namespace CCSWE.nanoFramework.Threading
         /// <summary>
         /// Waits for all the elements in the specified array to receive a signal, using an Int32 value to specify the time interval and specifying whether to exit the synchronization domain before the wait.
         /// </summary>
-        /// <param name="millisecondsTimeout">The number of milliseconds to wait, or Timeout.Infinite (-1) to wait indefinitely.</param>
+        /// <param name="timeout">The amount of time to wait, or <see cref="Timeout.InfiniteTimeSpan"/> (-1) to wait indefinitely.</param>
         /// <param name="exitContext">true to exit the synchronization domain for the context before the wait (if in a synchronized context), and reacquire it afterward; otherwise, false.</param>
         /// <param name="waitHandles">A WaitHandle array containing the objects for which the current instance will wait. This array cannot contain multiple references to the same object (duplicates).</param>
         /// <returns>true when every element in waitHandles has received a signal; otherwise, false.</returns>
-        public static bool WaitAll(int millisecondsTimeout, bool exitContext, params WaitHandle[] waitHandles)
+        public static bool WaitAll(TimeSpan timeout, bool exitContext, params WaitHandle[] waitHandles)
         {
-            return WaitHandle.WaitAll(waitHandles, millisecondsTimeout, exitContext);
+            return WaitAll((int)timeout.TotalMilliseconds, exitContext, waitHandles);
+        }
+
+        /// <summary>
+        /// Waits for any of the elements in the specified array to receive a signal, using a 32-bit signed integer to specify the time interval, and specifying whether to exit the synchronization domain before the wait.
+        /// </summary>
+        /// <param name="millisecondsTimeout">The number of milliseconds to wait, or <see cref="Timeout.Infinite"/> (-1) to wait indefinitely.</param>
+        /// <param name="exitContext">true to exit the synchronization domain for the context before the wait (if in a synchronized context), and reacquire it afterward; otherwise, false.</param>
+        /// <param name="waitHandles">A WaitHandle array containing the objects for which the current instance will wait.</param>
+        /// <returns>The array index of the object that satisfied the wait, or <see cref="WaitHandle.WaitTimeout"/> if no object satisfied the wait and a time interval equivalent to <paramref name="millisecondsTimeout"/> has passed.</returns>
+        public static int WaitAny(int millisecondsTimeout, bool exitContext, params WaitHandle[] waitHandles)
+        {
+            return WaitHandle.WaitAny(waitHandles, millisecondsTimeout, exitContext);
         }
 
         /// <summary>
@@ -42,13 +67,36 @@ namespace CCSWE.nanoFramework.Threading
         /// <summary>
         /// Waits for any of the elements in the specified array to receive a signal, using a 32-bit signed integer to specify the time interval, and specifying whether to exit the synchronization domain before the wait.
         /// </summary>
-        /// <param name="millisecondsTimeout">The number of milliseconds to wait, or Timeout.Infinite (-1) to wait indefinitely.</param>
+        /// <param name="timeout">The amount of time to wait, or <see cref="Timeout.InfiniteTimeSpan"/> (-1) to wait indefinitely.</param>
         /// <param name="exitContext">true to exit the synchronization domain for the context before the wait (if in a synchronized context), and reacquire it afterward; otherwise, false.</param>
         /// <param name="waitHandles">A WaitHandle array containing the objects for which the current instance will wait.</param>
-        /// <returns>The array index of the object that satisfied the wait, or WaitTimeout if no object satisfied the wait and a time interval equivalent to millisecondsTimeout has passed.</returns>
-        public static int WaitAny(int millisecondsTimeout, bool exitContext, params WaitHandle[] waitHandles)
+        /// <returns>The array index of the object that satisfied the wait, or <see cref="WaitHandle.WaitTimeout"/> if no object satisfied the wait and a time interval equivalent to <paramref name="millisecondsTimeout"/> has passed.</returns>
+        public static int WaitAny(TimeSpan timeout, bool exitContext, params WaitHandle[] waitHandles)
         {
-            return WaitHandle.WaitAny(waitHandles, millisecondsTimeout, exitContext);
+            return WaitAny((int)timeout.TotalMilliseconds, exitContext, waitHandles);
+        }
+
+        /// <summary>
+        /// Blocks the current thread until the current WaitHandle receives a signal, using a 32-bit signed integer to specify the time interval and specifying whether to exit the synchronization domain before the wait.
+        /// </summary>
+        /// <param name="waitHandle">The <see cref="WaitHandle"/> to wait on.</param>
+        /// <param name="millisecondsTimeout">The number of milliseconds to wait, or <see cref="Timeout.Infinite"/> (-1) to wait indefinitely.</param>
+        /// <returns>true if the current instance receives a signal; otherwise, false.</returns>
+        public static bool WaitOne(this WaitHandle waitHandle, int millisecondsTimeout)
+        {
+            return waitHandle.WaitOne(millisecondsTimeout, false);
+        }
+
+        /// <summary>
+        /// Blocks the current thread until the current WaitHandle receives a signal, using a 32-bit signed integer to specify the time interval and specifying whether to exit the synchronization domain before the wait.
+        /// </summary>
+        /// <param name="waitHandle">The <see cref="WaitHandle"/> to wait on.</param>
+        /// <param name="timeout">The amount of time to wait, or <see cref="Timeout.InfiniteTimeSpan"/> (-1) to wait indefinitely.</param>
+        /// <param name="exitContext">true to exit the synchronization domain for the context before the wait (if in a synchronized context), and reacquire it afterward; otherwise, false.</param>
+        /// <returns>true if the current instance receives a signal; otherwise, false.</returns>
+        public static bool WaitOne(this WaitHandle waitHandle, TimeSpan timeout, bool exitContext = false)
+        {
+            return waitHandle.WaitOne((int)timeout.TotalMilliseconds, exitContext);
         }
     }
 }
